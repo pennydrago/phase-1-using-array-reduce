1c50c55094da85cd3c2bc3586194df97a7602db8
Penelope Drago <prd@drago.com.au>
2023-03-23 15:52:06 +1100

    add variable

--- index.js
+++ index.js
@@ -1,3 +1,12 @@
 const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
 
 // Code your solution here
+
+// Variable to hold sum of all batteries
+
+const totalBatteries = batteryBatches.reduce(
+  (accumulator, currentCount) => accumulator + currentCount, 0
+);
+
+// Console log to test solution
+console.log(totalBatteries);
